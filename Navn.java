ad0b7d9a329c925ef45acf01dcffbe0c06b13990
Elise Hodnefjell <EliseHodnefjell@Elise-H.local>
2021-03-09 23:15:00 +0100

    første commit

---
+++ Navn.java
@@ -0,0 +1,15 @@
+/**
+ * Write a description of class Navn here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Navn
+{
+   public static void main(String args[])
+   {
+       System.out.println("Elise");
+}
+}
+
+
